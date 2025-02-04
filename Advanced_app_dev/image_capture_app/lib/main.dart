159c1ca651e42ab69f6cc1cfd80eaa0811cf42e4
Sudha75 <Sudhaps75@gmail.com>
2025-02-04 22:04:56 +0530

    Initial commit

---
+++ Advanced_app_dev/image_capture_app/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatefulWidget {
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  File? _image; // To store the image captured
+
+  // Function to pick an image from the camera
+  Future<void> _captureImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display the image if it is captured
+            _image == null ? Text('No image captured.') : Image.file(_image!),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _captureImage,
+              child: Text('Capture Image'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
